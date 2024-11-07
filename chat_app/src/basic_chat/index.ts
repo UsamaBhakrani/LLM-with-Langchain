5edf1b75b85e303a69d86729cd5759919840723c
Usama Bhakrani <usamabhakrani@gmail.com>
2024-11-07 23:16:01 +0500

    encoder and Max tokens

--- chat_app/src/basic_chat/index.ts
+++ chat_app/src/basic_chat/index.ts
@@ -1,6 +1,9 @@
 import OpenAI from "openai";
+import { encoding_for_model } from "tiktoken";
 
 const openai = new OpenAI();
+const encoder = encoding_for_model("gpt-3.5-turbo");
+const MAX_TOKENS = 700;
 
 const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
   { role: "system", content: "You are a helpful chatbot" },
