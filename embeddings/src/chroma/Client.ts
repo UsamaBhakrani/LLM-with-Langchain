752bfe411d7bc3c246e3048459c7a6f403e8f50c
Usama Bhakrani <usamabhakrani@gmail.com>
2024-11-11 23:12:39 +0500

    OpenAi Embeddings in ChromaDB

--- embeddings/src/chroma/Client.ts
+++ embeddings/src/chroma/Client.ts
@@ -1,16 +1,36 @@
-import { ChromaClient } from "chromadb";
+import { ChromaClient, OpenAIEmbeddingFunction } from "chromadb";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const client = new ChromaClient({
   path: "http://localhost:8000",
 });
 
+const embeddingFunction = new OpenAIEmbeddingFunction({
+  openai_api_key: process.env.OPENAI_API_KEY!,
+  openai_model: "text-embedding-3-small",
+});
+
 const main = async () => {
-  //   const response = await client.createCollection({ name: "data-test" });
   await client.api
     .createTenant({ name: "Usama" })
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
-  //   console.log(response);
 };
 
-main();
+const addData = async () => {
+  const collection = await client.getCollection({
+    name: "data-test",
+    embeddingFunction,
+  });
+  const result = await collection.add({
+    ids: ["id0"],
+    documents: ["Here is my entry"],
+    embeddings: [[0.1, 0.2]],
+  });
+
+  console.log(result);
+};
+
+addData();
