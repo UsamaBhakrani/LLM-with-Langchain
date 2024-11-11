2b770276c76d42ea9ce1b1477df21ebe455bd81d
Usama Bhakrani <usamabhakrani@gmail.com>
2024-11-11 20:06:12 +0500

    Embeddings

--- pycode/main.py
+++ pycode/main.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import sequential
+
 import argparse
 
 load_dotenv()
@@ -11,14 +14,24 @@ parser.add_argument('--language', default='python')
 args = parser.parse_args()
 
 llm = ChatOpenAI()
+
+
 code_prompt = ChatPromptTemplate.from_messages([
     ('user',
      'Write a code snippet in {language} that will {task}')
 ])
 
-code_chain = (code_prompt | llm)
+test_prompt = ChatPromptTemplate.from_messages(
+    ['user', 'Write a a test for the following {language} code:\n{code}']
+)
 
-result = code_chain.invoke(
+code_chain = (code_prompt | llm)
+test_chain = (test_prompt | llm)
+
+chain = code_chain | test_chain
+
+result = chain.invoke(
     {'language': args.language, 'task': args.task})
 
-print(result.content)
+
+print(result)
